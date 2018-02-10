7dea38add220bce9f160e5e473e013fbbd14e3cf
Rud156 <rudrangshu_1997@yahoo.com>
2018-02-10 12:32:19 +0530

    Fixed And Updated With UIKit

--- client/src/app.ts
+++ client/src/app.ts
@@ -1,3 +1,12 @@
+import "./../assets/UIKit/css/uikit.min.css";
+import * as UIkit from "./../assets/UIKit/js/uikit";
+
+import "./../assets/font-awesome/js/fontawesome-all";
+
 export class App {
-  message = 'Hello World!';
+  message = "Hello World!";
+
+  attached() {
+    UIkit.notification("Hello World");
+  }
 }
