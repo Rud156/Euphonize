6f19cfe898d1bc70aa8e8d0368315f547ac6c19f
Rud156 <rudrangshu_1997@yahoo.com>
2018-02-18 12:51:07 +0530

    Css Upload

--- client/src/app.ts
+++ client/src/app.ts
@@ -3,9 +3,39 @@ import * as UIkit from 'uikit';
 import 'fontawesome';
 
 export class App {
-  message = 'Hello World!';
+  sidebarRef: HTMLElement;
+  sidebarShowing: boolean = false;
+  searchString: string = '';
 
   attached() {
-    UIkit.notification('Hello World');
+    UIkit.offcanvas(this.sidebarRef).hide();
+
+    UIkit.util.on(this.sidebarRef, 'hidden', () => {
+      this.closeSidebar();
+    });
+  }
+
+  detached() {
+    // @ts-ignore
+    this.sidebarRef.$destroy();
+  }
+
+  closeSidebar() {
+    this.sidebarShowing = false;
+  }
+
+  searchSubmitted() {
+    console.log(this.searchString);
+    this.searchString = '';
+  }
+
+  toggleSidebar() {
+    if (this.sidebarShowing) {
+      UIkit.offcanvas(this.sidebarRef).hide();
+      this.sidebarShowing = false;
+    } else {
+      UIkit.offcanvas(this.sidebarRef).show();
+      this.sidebarShowing = true;
+    }
   }
 }
