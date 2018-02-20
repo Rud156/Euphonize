ca4ebb706fddc0df7f4a59d179c56f9af215f51e
Rud156 <rudrangshu_1997@yahoo.com>
2018-02-20 13:33:26 +0530

    Added Top Tracks

--- client/src/components/tracks/index.ts
+++ client/src/components/tracks/index.ts
@@ -1,12 +1,35 @@
+import { inject } from 'aurelia-framework';
+
 // @ts-ignore
 import * as UIkit from 'uikit';
 
+import TrackService from '../../common/services/trackService';
+import { CONTENT_TYPES } from '../../common/utils/constants';
+
+@inject(TrackService)
 export class Tracks {
   sliderTracks: HTMLElement;
+  topTracks = [];
 
-  constructor() {}
+  constructor(private trackService: TrackService) {}
 
   attached() {
     UIkit.slider(this.sliderTracks);
+    this.fetchTopTracks();
+  }
+
+  fetchTopTracks() {
+    this.trackService
+      .getTopTracks(CONTENT_TYPES.USER_CU)
+      .then(data => {
+        if (data.success) {
+          this.topTracks = data.tracks;
+        } else {
+          window.alert('Error Occurred');
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 }
