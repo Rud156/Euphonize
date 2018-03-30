e8f87791da03397798b814a1bc8f8aedcee98a9e
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2018-03-30 08:33:51 +0530

    Fixes #1

--- client/src/resources/custom-attributes/source.ts
+++ client/src/resources/custom-attributes/source.ts
@@ -1,21 +1,29 @@
 import { customAttribute, inject } from 'aurelia-framework';
 import { TRACK_IMAGE_PLACEHOLDER } from '../../common/utils/constants';
 
-@inject(HTMLImageElement)
+@inject(Element)
 export class SourceCustomAttribute {
   value: any;
   imageSource: string = '';
   eventListenerRemoved: boolean = false;
 
-  constructor(private element: HTMLImageElement) {}
+  constructor(private element: Element) {}
 
   bind() {
     this.imageSource = this.value;
+    // @ts-ignore
+    this.element.src = this.imageSource;
+  }
+
+  valueChanged() {
+    this.imageSource = this.value;
+    // @ts-ignore
     this.element.src = this.imageSource;
   }
 
   handleImageError() {
     this.element.removeEventListener('error', this.handleImageError);
+    // @ts-ignore
     this.element.src = TRACK_IMAGE_PLACEHOLDER;
     this.eventListenerRemoved = true;
   }
